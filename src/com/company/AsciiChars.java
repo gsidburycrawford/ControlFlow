1ad2fb55d91f2f5814f44a6b5e12056e17d405da
gsidburycrawford <geterry12345@gmail.com>
2021-04-14 16:23:26 -0400

    Initial commit

---
+++ src/com/company/AsciiChars.java
@@ -0,0 +1,26 @@
+package com.company;
+public class AsciiChars {
+    public static void printNumbers()
+    {
+        for(int i = 48; i < 58; i++){
+            System.out.print((char)i + " ");
+        }
+        System.out.println();
+
+    }
+    public static void printLowerCase()
+    {
+        for(int i = 97; i < 123; i++){
+            System.out.print((char)i + " ");
+        }
+        System.out.println();
+    }
+    public static void printUpperCase()
+    {
+        for(int i = 65; i < 91; i++){
+            System.out.print((char)i + " ");
+        }
+        System.out.println();
+
+    }
+}
